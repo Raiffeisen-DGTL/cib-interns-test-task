a97232bd9ba7e9dc68498e19ecd2359776238676
Konstantin <alaevdev@gmail.com>
2021-10-02 17:10:34 +0300

    update exception

--- src/main/java/alaev/dev/raiffeisentesttask/exception/InvalidCottonPart.java
+++ src/main/java/alaev/dev/raiffeisentesttask/exception/InvalidCottonPart.java
@@ -3,6 +3,8 @@ package alaev.dev.raiffeisentesttask.exception;
 public class InvalidCottonPart extends RuntimeException {
 
   public InvalidCottonPart(String message) {
-    super(message);
+    super(String.format(
+        "Percentage of cotton (in the composition of socks from 0 to 100. You receive: %s",
+        message));
   }
 }
--- src/main/java/alaev/dev/raiffeisentesttask/exception/InvalidQuantity.java
+++ src/main/java/alaev/dev/raiffeisentesttask/exception/InvalidQuantity.java
@@ -3,6 +3,7 @@ package alaev.dev.raiffeisentesttask.exception;
 public class InvalidQuantity extends RuntimeException {
 
   public InvalidQuantity(String message) {
-    super(message);
+    super(String.format("The number of pairs of socks, an integer greater than 0. You receive: %s",
+                        message));
   }
 }
