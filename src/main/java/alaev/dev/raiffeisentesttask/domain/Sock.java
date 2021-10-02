4e1201467c43a703ece9b3384c667dbf964d212b
Konstantin <alaevdev@gmail.com>
2021-10-02 14:38:57 +0300

    add entity sock

---
+++ src/main/java/alaev/dev/raiffeisentesttask/domain/Sock.java
@@ -0,0 +1,20 @@
+package alaev.dev.raiffeisentesttask.domain;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity (name = "socks")
+public class Sock {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "color")
+    private String color;
+    
+    @Column(name = "cotton_part")
+    private Byte cottonPart;
+}
