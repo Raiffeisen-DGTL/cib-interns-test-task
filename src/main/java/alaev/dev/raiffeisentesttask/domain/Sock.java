fde91d062a66a88affdf9ee0aa801a5861e3b374
Konstantin <alaevdev@gmail.com>
2021-10-02 14:51:21 +0300

    fix code style

--- src/main/java/alaev/dev/raiffeisentesttask/domain/Sock.java
+++ src/main/java/alaev/dev/raiffeisentesttask/domain/Sock.java
@@ -1,20 +1,19 @@
 package alaev.dev.raiffeisentesttask.domain;
 
-import lombok.Data;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity (name = "socks")
+@Entity(name = "socks")
 public class Sock {
-    @Id
-    @Column(name = "id")
-    private Long id;
 
-    @Column(name = "color")
-    private String color;
-    
-    @Column(name = "cotton_part")
-    private Byte cottonPart;
+  @Id
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "color")
+  private String color;
+
+  @Column(name = "cotton_part")
+  private Byte cottonPart;
 }
