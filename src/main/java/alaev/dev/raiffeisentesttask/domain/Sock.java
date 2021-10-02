ac9db458e10efa388901a6a243fe2a7b8693174a
Konstantin <alaevdev@gmail.com>
2021-10-02 16:31:03 +0300

    fix domain sock

--- src/main/java/alaev/dev/raiffeisentesttask/domain/Sock.java
+++ src/main/java/alaev/dev/raiffeisentesttask/domain/Sock.java
@@ -1,10 +1,21 @@
 package alaev.dev.raiffeisentesttask.domain;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity(name = "socks")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Sock {
 
   @Id
@@ -15,5 +26,28 @@ public class Sock {
   private String color;
 
   @Column(name = "cotton_part")
-  private Byte cottonPart;
+  private Integer cottonPart;
+
+  @Column(name = "quantity")
+  private Integer quantity;
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Sock sock = (Sock) o;
+    return Objects.equals(id, sock.id) && Objects.equals(color, sock.color)
+        && Objects.equals(cottonPart, sock.cottonPart) && Objects.equals(quantity,
+                                                                         sock.quantity);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
