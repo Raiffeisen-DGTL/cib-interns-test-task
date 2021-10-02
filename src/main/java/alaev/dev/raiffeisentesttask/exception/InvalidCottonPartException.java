15ecef85b560f6ccb78502566c38c165baff269e
Konstantin <alaevdev@gmail.com>
2021-10-02 18:09:01 +0300

    fix text message in InvalidCottonPartException

--- src/main/java/alaev/dev/raiffeisentesttask/exception/InvalidCottonPartException.java
+++ src/main/java/alaev/dev/raiffeisentesttask/exception/InvalidCottonPartException.java
@@ -4,7 +4,7 @@ public class InvalidCottonPartException extends RuntimeException {
 
   public InvalidCottonPartException(String message) {
     super(String.format(
-        "Percentage of cotton (in the composition of socks from 0 to 100. You receive: %s",
+        "Percentage of cotton in the composition of socks from 0 to 100. You receive: %s",
         message));
   }
 }
