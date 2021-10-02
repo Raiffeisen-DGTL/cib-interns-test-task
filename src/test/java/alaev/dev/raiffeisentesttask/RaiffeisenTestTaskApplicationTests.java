1d4c24a8912d35dfc603fb48390c0d562d423134
Konstantin <alaevdev@gmail.com>
2021-10-02 11:54:50 +0300

    init commit

---
+++ src/test/java/alaev/dev/raiffeisentesttask/RaiffeisenTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package alaev.dev.raiffeisentesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiffeisenTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
