90dbf40d968091c4ccad8137b525af04a356253b
Konstantin <alaevdev@gmail.com>
2021-10-02 17:10:54 +0300

    add test for controller

---
+++ src/test/java/alaev/dev/raiffeisentesttask/controller/ControllerTest.java
@@ -0,0 +1,55 @@
+package alaev.dev.raiffeisentesttask.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import alaev.dev.raiffeisentesttask.exception.InvalidCottonPart;
+import alaev.dev.raiffeisentesttask.exception.InvalidQuantity;
+import alaev.dev.raiffeisentesttask.service.SockService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+@Import({Controller.class, SockService.class})
+@SpringBootTest
+class ControllerTest {
+
+  public static final int INVALID_COTTON_PART = 101;
+  public static final int QUANTITY = 0;
+  public static final int COTTON_PART = 10;
+  public static final int INVALID_QUANTITY = -1;
+  @Autowired
+  private Controller controller;
+
+  @MockBean
+  private SockService service;
+
+  @Test
+  void shouldContextLoads() {
+    assertThat(controller).isNotNull();
+    assertThat(service).isNotNull();
+  }
+
+  @Test
+  void shouldThrowInvalidCottonPartException() {
+    assertThatThrownBy(() -> controller.registerArrivalSocks("", INVALID_COTTON_PART, QUANTITY))
+        .isInstanceOf(InvalidCottonPart.class);
+
+    verify(service, times(0))
+        .addSock(any(), any(), any());
+  }
+
+  @Test
+  void shouldThrowInvalidQuantityException() {
+    assertThatThrownBy(() -> controller.registerArrivalSocks("", COTTON_PART, INVALID_QUANTITY))
+        .isInstanceOf(InvalidQuantity.class);
+
+    verify(service, times(0))
+        .addSock(any(), any(), any());
+  }
+}
