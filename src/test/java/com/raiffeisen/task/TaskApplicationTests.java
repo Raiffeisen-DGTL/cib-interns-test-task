900f2a34b0d9fbd83ebcf159136620eaefb4353a
Aleksei <60744232+RussianRightWing@users.noreply.github.com>
2021-10-03 17:18:11 +0300

    Initial commit

---
+++ src/test/java/com/raiffeisen/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raiffeisen.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
