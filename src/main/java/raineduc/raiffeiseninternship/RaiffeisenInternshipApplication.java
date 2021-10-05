a37abd39c99befde1f8f1d8b61309f825e9aa69c
raineduc <hramil0120@gmail.com>
2021-10-05 15:26:31 +0300

    Init project

---
+++ src/main/java/raineduc/raiffeiseninternship/RaiffeisenInternshipApplication.java
@@ -0,0 +1,13 @@
+package raineduc.raiffeiseninternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiffeisenInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaiffeisenInternshipApplication.class, args);
+    }
+
+}
