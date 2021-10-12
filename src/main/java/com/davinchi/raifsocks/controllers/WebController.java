10e458bc8ccfa9f7d4f74e3a49e306bfb39d068b
Da_Vinchi <igselyak@gmail.com>
2021-10-12 16:28:02 +0300

    MVP version

--- src/main/java/com/davinchi/raifsocks/controllers/WebController.java
+++ src/main/java/com/davinchi/raifsocks/controllers/WebController.java
@@ -2,16 +2,17 @@ package com.davinchi.raifsocks.controllers;
 
 import com.davinchi.raifsocks.Socks;
 import com.davinchi.raifsocks.repos.SocksRepo;
+import org.hibernate.criterion.Example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.relational.core.sql.In;
+import org.springframework.expression.ParseException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import java.util.List;
 import java.util.Objects;
 
@@ -21,46 +22,72 @@ public class WebController {
     @Autowired
     private SocksRepo socksRepo;
 
-    @PostMapping("/api/socks/income")
-    public ResponseEntity incomeSocks(@RequestBody Socks socks){
-        Socks oldSocks = socksRepo.findByColorAndCottonPart(socks.getColor(),socks.getCottonPart()).get(0);
-        oldSocks.setQuantity(oldSocks.getQuantity()+socks.getQuantity());
+
+    @RequestMapping(value ="/api/socks/income", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> incomeSocks(@RequestBody Socks socks) throws ParseException {
+        System.out.print(socks.getQuantity());
+        Socks oldSocks;
+        try {
+            oldSocks = socksRepo.findByColorAndCottonPart(socks.getColor(), socks.getCottonPart()).get(0);
+            oldSocks.setQuantity(oldSocks.getQuantity()+socks.getQuantity());
+        } catch (Exception e){
+            oldSocks = socks;
+        }
         socksRepo.save(oldSocks);
-        return new ResponseEntity(HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("/api/socks/outcome")
-    public ResponseEntity outcomeSocks(@RequestBody Socks socks){
-        Socks oldSocks = socksRepo.findByColorAndCottonPart(socks.getColor(),socks.getCottonPart()).get(0);
-        oldSocks.setQuantity(oldSocks.getQuantity()-socks.getQuantity());
-        socksRepo.save(oldSocks);
-        return new ResponseEntity(HttpStatus.ACCEPTED);
+    @RequestMapping(value ="/api/socks/outcome", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> outcomeSocks(@RequestBody Socks socks) throws ParseException {
+        Socks oldSocks;
+        try {
+            oldSocks = socksRepo.findByColorAndCottonPart(socks.getColor(), socks.getCottonPart()).get(0);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Quantity is too much", HttpStatus.BAD_REQUEST);
+        }
+        int temp = oldSocks.getQuantity() - socks.getQuantity();
+        if (temp >= 0) {
+            oldSocks.setQuantity(temp);
+            socksRepo.save(oldSocks);
+        } else {
+            return new ResponseEntity<>("Quantity is too much", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/api/socks")
-    public String getSocksQuantity(@RequestParam String color, @RequestParam String operation, @RequestParam Integer cottonPart){
-        List<Socks> socksList = socksRepo.findByColor(color);
-        Integer quantity = 0;
-        for (Socks socks: socksList)
-        {
+        @RequestMapping(value ="/api/socks", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> getSocksQuantity(@RequestParam String color, @RequestParam String operation, @RequestParam Integer cottonPart){
+        int quantity = 0;
+        List<Socks> socksList;
          switch (operation){
              case "moreThan":
-                 if (socks.getQuantity() > cottonPart)
-                 {
-                     quantity += socks.getQuantity();
-                 }
+                 socksList = socksRepo.findByColorAndCottonPartIsGreaterThan(color, cottonPart);
+                 break;
              case "lessThan":
-                 if (socks.getQuantity() < cottonPart)
-                 {
-                     quantity += socks.getQuantity();
-                 }
+                 socksList = socksRepo.findByColorAndCottonPartIsLessThan(color, cottonPart);
+                 break;
              case "equal":
-                 if (Objects.equals(socks.getQuantity(), cottonPart))
-                 {
-                     quantity += socks.getQuantity();
-                 }
+                 socksList = socksRepo.findByColorAndCottonPartIs(color, cottonPart);
+                 break;
+             default:
+                 throw new IllegalStateException("Unexpected value: " + operation);
          }
+        for (Socks socks:socksList) {
+            quantity = quantity + socks.getQuantity();
         }
-        return quantity.toString();
+        return new ResponseEntity<>(Integer.toString(quantity), HttpStatus.OK);
     }
+
+    @ExceptionHandler(IllegalStateException.class)
+        public ResponseEntity<?> badMethod(IllegalStateException illegalStateException){
+            return new ResponseEntity<>("WRONG METHOD",HttpStatus.BAD_REQUEST);
+        }
+
+    @ExceptionHandler(ParseException.class)
+    public ResponseEntity<?> badObject(ParseException parseException){
+        return new ResponseEntity<>("WRONG DATA",HttpStatus.BAD_REQUEST);
+        }
 }
--- src/main/java/com/davinchi/raifsocks/repos/SocksRepo.java
+++ src/main/java/com/davinchi/raifsocks/repos/SocksRepo.java
@@ -1,6 +1,7 @@
 package com.davinchi.raifsocks.repos;
 
 import com.davinchi.raifsocks.Socks;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface SocksRepo extends CrudRepository<Socks,Long> {
     List<Socks> findByColor(String color);
     List<Socks> findByCottonPart(Integer cottonPart);
     List<Socks> findByColorAndCottonPart(String color, Integer cottonPart);
+    List<Socks> findByColorAndCottonPartIsGreaterThan(String color, Integer cottonPart);
+    List<Socks> findByColorAndCottonPartIsLessThan(String color, Integer cottonPart);
+    List<Socks> findByColorAndCottonPartIs(String color, Integer cottonPart);
 }
