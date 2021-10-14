e51129ca65cdd047f4e4e30d2c10263550e3b23c
Igor Mihailov <dr.migo@ya.ru>
2021-10-14 20:54:40 +0300

    Task for review

---
+++ src/test/java/com/accounting/sock/SockApplicationTests.java
@@ -0,0 +1,10 @@
+package com.accounting.sock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockApplicationTests {
+
+
+}
