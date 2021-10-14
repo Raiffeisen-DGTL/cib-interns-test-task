ca7034349c2968b7ba2fdee5260a8ae29217b8be
Рома <4rl1996@gmail.com>
2021-10-14 21:33:20 +0300

    add .gitginore

--- target/classes/db/migration/V1__init.sql
+++
@@ -1,7 +0,0 @@
-create table socks(
-    id bigserial primary key,
-    color varchar(30) not null,
-    cotton_part integer not null check ( cotton_part >= 0 and cotton_part <= 100 ),
-    quantity bigint not null check ( quantity >= 0 ),
-    constraint color_cottonPart_unique UNIQUE(color, cotton_part)
-);
