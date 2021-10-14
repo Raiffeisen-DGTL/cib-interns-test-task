8d96686e0f0ffe3f2efc832c9435c2d11892cc8c
tema9984 <47312875+tema9984@users.noreply.github.com>
2021-10-14 21:43:57 +0300

    Delete target directory

--- target/classes/db/migration/V000001_add_test_data_to_socks.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE socks (
-                         id int NOT NULL AUTO_INCREMENT,
-                         color varchar(50) NOT NULL,
-                         cottonPart int NOT NULL ,
-                         quantity int NOT NULL,
-                         PRIMARY KEY (id)
-);
