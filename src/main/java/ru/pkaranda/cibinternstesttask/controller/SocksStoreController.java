4eaa514a0826184f4965cac7b49f9a5865625ec0
karanda-p <77533722+karanda-p@users.noreply.github.com>
2021-10-14 12:17:00 +0300

    Add some files

---
+++ src/main/java/ru/pkaranda/cibinternstesttask/controller/SocksStoreController.java
@@ -0,0 +1,10 @@
+package ru.pkaranda.cibinternstesttask.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SocksStoreController {
+
+
+
+}
---
+++ src/test/java/ru/pkaranda/cibinternstesttask/CibInternsTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pkaranda.cibinternstesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibInternsTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
