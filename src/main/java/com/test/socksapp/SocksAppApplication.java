0b8b1d0a3750e6267669c54510edc699d9b50a0c
alina <lina.y2018@ya.ru>
2021-10-14 20:40:34 +0300

    init commit

---
+++ src/main/java/com/test/socksapp/SocksAppApplication.java
@@ -0,0 +1,13 @@
+package com.test.socksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksAppApplication.class, args);
+    }
+
+}
