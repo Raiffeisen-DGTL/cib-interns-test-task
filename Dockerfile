769cd679be1b446febb7f51ac654588f4deea7a1
Leonid Zhmenko <44261808+ZhmenkoLeonid@users.noreply.github.com>
2021-10-15 13:20:41 +0600

    init

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,1 @@
 FROM postgres:latest
-RUN apt-get --force-yes -y update && apt-get --force-yes -y install openjdk-11-jre
-ARG JAR_FILE=*.jar
-COPY target/${JAR_FILE} application.jar
-EXPOSE 8080
-EXPOSE 5432
-#ENTRYPOINT ["java", "-jar","/application.jar"]
--- src/main/java/com/raifaisentask/ApplicationConfig.java
+++ src/main/java/com/raifaisentask/ApplicationConfig.java
@@ -29,9 +29,10 @@ public class ApplicationConfig {
     public DataSource dataSource() throws IOException {
         DriverManagerDataSource source = new DriverManagerDataSource();
         source.setDriverClassName(org.postgresql.Driver.class.getName());
-        source.setUrl("jdbc:postgresql://localhost:5432/postgres");
-        source.setUsername("postgres");
-        source.setPassword("4HZdKL");
+        source.setUrl("jdbc:postgresql://\n" +
+                "ec2-54-216-48-43.eu-west-1.compute.amazonaws.com:5432/dfsht4lreg16br");
+        source.setUsername("cgjbktugedldbr");
+        source.setPassword("c3e6e6e9172a5aff074353c1ec31b5fef6259289b0d4a76687348669230a852e");
         Properties properties = new Properties();
 
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
