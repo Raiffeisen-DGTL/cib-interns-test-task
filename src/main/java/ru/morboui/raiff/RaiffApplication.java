4ec803af1e7d53a2fb2071d3d2b0b288193f5eb2
Dmitry B <lbvf6000@gmail.com>
2021-10-15 17:10:44 +0300

    Initial commit

---
+++ src/main/java/ru/morboui/raiff/RaiffApplication.java
@@ -0,0 +1,13 @@
+package ru.morboui.raiff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiffApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RaiffApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/morboui/raiff/RaiffApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.morboui.raiff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiffApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
