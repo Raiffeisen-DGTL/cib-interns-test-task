38a4a12faa91adce79fe7c98dac47cd5a31b6263
karanda-p <77533722+karanda-p@users.noreply.github.com>
2021-10-15 23:54:21 +0300

    Final changes #2

--- src/main/java/ru/pkaranda/cibinternstesttask/mapper/common/SocksTransactionMapper.java
+++ src/main/java/ru/pkaranda/cibinternstesttask/mapper/common/SocksTransactionMapper.java
@@ -10,13 +10,5 @@ import ru.pkaranda.cibinternstesttask.model.dto.SocksTransactionDTO;
 @Mapper(componentModel = "spring")
 public interface SocksTransactionMapper extends BaseMapper<SocksTransaction, SocksTransactionDTO> {
 
-    @Override
-    @Mappings({
-            @Mapping(source = "domain.id", target = "id"),
-            @Mapping(source = "domain.color", target = "color"),
-            @Mapping(source = "domain.transactionType", target = "transactionType"),
-            @Mapping(source = "domain.cottonPart", target = "cottonPart"),
-            @Mapping(source = "domain.quantity", target = "quantity")
-    })
     SocksTransactionDTO domainToDto(SocksTransaction domain);
 }
