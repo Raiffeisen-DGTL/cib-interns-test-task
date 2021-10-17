f29afef9bd970acc5de75441415e91944543a768
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-10-17 14:33:47 +0300

    Добавил application

--- src/main/java/com/example/bootcamp/service/GetSocksService.java
+++ src/main/java/com/example/bootcamp/service/GetSocksService.java
@@ -7,5 +7,13 @@ import java.util.List;
 
 public interface GetSocksService {
 
+    /**
+     *
+     * @param color необходимый цвет пар носков
+     * @param operation оператор сравнения значения количества хлопка в составе носков,
+     *                  одно значение из: moreThan, lessThan, equal
+     * @param cotton значение процента хлопка в составе носков из сравнения
+     * @return общее количество носков на складе, соответствующих переданным в параметрах критериям запроса
+     */
     int getSocks(String color, Operation operation, short cotton);
 }
