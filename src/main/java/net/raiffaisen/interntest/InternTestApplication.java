15dd596bad94a8e8baff7e78c762260b4b5bbabb
Sunat2001 <sunnat-foto@mail.ru>
2021-10-17 19:04:22 +0500

    start project

---
+++ src/main/java/net/raiffaisen/interntest/InternTestApplication.java
@@ -0,0 +1,13 @@
+package net.raiffaisen.interntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/raiffaisen/interntest/InternTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.raiffaisen.interntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
