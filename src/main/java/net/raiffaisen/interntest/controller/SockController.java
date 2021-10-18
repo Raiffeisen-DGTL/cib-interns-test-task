d9b1dd652dd50d93cb79f8b3bcfa8f301d341bcd
Sunat2001 <sunnat-foto@mail.ru>
2021-10-18 10:02:31 +0500

    Modified

--- src/main/java/net/raiffaisen/interntest/controller/SockController.java
+++ src/main/java/net/raiffaisen/interntest/controller/SockController.java
@@ -1,4 +1,24 @@
 package net.raiffaisen.interntest.controller;
 
+import java.util.List;
+
+import net.raiffaisen.interntest.model.Sock;
+import net.raiffaisen.interntest.repository.SockRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+//@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
 public class SockController {
+
+    @Autowired
+    SockRepository sockRepository;
+
+    @GetMapping("/index")
+    public List<Sock> getAllTutorials() {
+        return sockRepository.findAll();
+    }
 }
