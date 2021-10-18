7f3b28fbdc739367d676106dddb97615db6213d7
L-san <lsarelli9402@gmail.com>
2021-10-18 17:28:36 +0300

    project structure

---
+++ src/test/java/ru/lsan/cibinternstesttask/CibInternsTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lsan.cibinternstesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibInternsTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
