7a6718268502f918b20c1c0b49f1b2d5475377a2
s.arslanov <s.arslanov@innopolis.university>
2021-10-18 18:15:51 +0300

    Working locally

--- RESTServer/src/main/java/com/example/RESTServer/RestServerApplication.java
+++ RESTServer/src/main/java/com/example/RESTServer/RestServerApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RestServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestServerApplication.class, args);
+    }
 
 }
