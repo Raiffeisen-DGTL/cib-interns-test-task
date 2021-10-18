bbae6c35bb748a199375f69bf2eb0e325b194fd4
s.arslanov <s.arslanov@innopolis.university>
2021-10-18 12:54:53 +0300

    Working locally, now working on docker-compose.yml

---
+++ RESTServer/src/main/java/com/example/RESTServer/RestServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.RESTServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServerApplication.class, args);
+	}
+
+}
---
+++ RESTServer/src/main/java/com/example/RESTServer/service/SockService.java
@@ -0,0 +1,4 @@
+package com.example.RESTServer.service;
+
+public class SockService {
+}
