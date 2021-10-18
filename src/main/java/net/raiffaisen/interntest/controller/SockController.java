25f0fa56b2d1330acfe80804ebc6e3eccbed176f
Sunat2001 <sunnat-foto@mail.ru>
2021-10-18 13:32:13 +0500

    Controller updates

--- src/main/java/net/raiffaisen/interntest/controller/SockController.java
+++ src/main/java/net/raiffaisen/interntest/controller/SockController.java
@@ -1,11 +1,13 @@
 package net.raiffaisen.interntest.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.raiffaisen.interntest.model.Sock;
 import net.raiffaisen.interntest.repository.SockRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +20,18 @@ public class SockController {
     SockRepository sockRepository;
 
     @GetMapping("/index")
-    public List<Sock> getAllTutorials() {
-        return sockRepository.findAll();
+    public List<Sock> getAllSocks(){
+        try {
+            List<Sock> socks = new ArrayList<Sock>();
+
+               return sockRepository.findAll();
+
+            if (socks.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(socks, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 }
