b6717348f10b4bb7e73cec27de4ce6e1c549b952
macodes <m.macodes@mail.ru>
2021-10-20 07:02:36 +0700

    Initial commit

---
+++ src/test/java/ru/macodes/raiffeisenapi/RaiffeisenTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.macodes.raiffeisenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiffeisenTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
