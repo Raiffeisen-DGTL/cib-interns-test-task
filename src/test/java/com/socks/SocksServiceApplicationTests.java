8ee0f0a206864fc655006dfe392fe735e834dca2
AMRomanov <am-romanov@mail.ru>
2021-10-21 17:56:06 +0500

    Init commit

---
+++ src/test/java/com/socks/SocksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
