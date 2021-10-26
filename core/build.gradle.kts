b7f9513392d8d9623c3df57118bd55b453dc02e1
a.Ilinykh <a.ilinykh@unabank.com>
2021-10-26 09:49:54 +0500

    add mapstruck and validator

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -17,6 +17,14 @@ dependencies {
     //database
     //implementation("org.liquibase:liquibase-core")
     implementation("org.postgresql:postgresql")
+
+    implementation("org.hibernate.validator:hibernate-validator:6.0.13.Final")
+    implementation("jakarta.validation:jakarta.validation-api:2.0.2")
+
+    // Mapstruct
+    kapt("org.mapstruct:mapstruct-processor:1.4.0.Final")
+    implementation("org.mapstruct:mapstruct-processor:1.4.0.Final")
+    implementation("org.mapstruct:mapstruct:1.4.0.Final")
 }
 repositories {
     mavenCentral()
