b2683223d3a8b532597112bf5d09716447fd3ced
Andrey <AK20001701@yandex.ru>
2021-10-26 12:25:50 +0300

    initial commit

---
+++ socks/src/test/java/ru/raiffeisen/socks/SocksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.raiffeisen.socks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
