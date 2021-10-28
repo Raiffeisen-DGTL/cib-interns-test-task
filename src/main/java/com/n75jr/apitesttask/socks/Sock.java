efd0074b2a288054d8d7d098d529257ab47357d4
Roman Nikitin <n75rom@gmail.com>
2021-10-29 00:46:36 +0900

    Add POJO Sock

---
+++ src/main/java/com/n75jr/apitesttask/socks/Sock.java
@@ -0,0 +1,67 @@
+package com.n75jr.apitesttask.socks;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "socks")
+public class Sock {
+    @Id
+//    @Transient
+    @Column(name = "id")
+    //@SequenceGenerator(name = "socksIdSeq", sequenceName = "socks_id_seq", allocationSize = 1)
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "socksIdSeq")
+    private long id;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "cotton_part")
+    private int cottonPart;
+
+    @Column(name = "timestamp")
+    private LocalDate date;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getCottonPart() {
+        return cottonPart;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCottonPart(int cottonPart) {
+        this.cottonPart = cottonPart;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Sock{" +
+                "id=" + id +
+                ", color='" + color + '\'' +
+                ", cottonPart=" + cottonPart +
+                ", date=" + date +
+                '}';
+    }
+}
