41182b0cdeaf6e1d1001f460cd98d5eaf7598e68
Roman Nikitin <n75rom@gmail.com>
2021-10-29 00:23:54 +0900

    Initial commit

---
+++ src/main/java/com/n75jr/apitesttask/ApiTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.n75jr.apitesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/n75jr/apitesttask/ApiTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n75jr.apitesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
