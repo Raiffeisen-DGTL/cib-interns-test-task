c28c0f256bd8242fd9e6375c77b40f1d3e60cd4c
Roman Nikitin <n75rom@gmail.com>
2021-10-29 00:49:18 +0900

    Implement interface of service for the project

---
+++ src/main/java/com/n75jr/apitesttask/socks/SockServiceImpl.java
@@ -0,0 +1,60 @@
+package com.n75jr.apitesttask.socks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+public class SockServiceImpl implements SockService {
+    private final SockRepository sockRepository;
+
+    @Autowired
+    public SockServiceImpl(SockRepository sockRepository) {
+        this.sockRepository = sockRepository;
+    }
+
+    @Override
+    public long getSize() {
+        return sockRepository.count();
+    }
+
+    @Override
+    public void income(Sock sock) {
+        System.out.println("id: " + sock.getId());
+        sockRepository.save(sock);
+    }
+
+    @Override
+    public void outcome(Sock sock) {
+        System.out.println("id: " + sock.getId());
+        sockRepository.delete(sock);
+    }
+
+    @Override
+    public void outcomeWithoutId(String color, int cotton_part) {
+        System.out.println(sockRepository.outcomeWithoutId(color, cotton_part));
+        sockRepository.deleteAllById(sockRepository.outcomeWithoutId(color, cotton_part));
+    }
+
+    @Override
+    public int operMoreThan(String color, int cotton_part) {
+        return sockRepository.operMoreThan(color, cotton_part);
+    }
+
+    @Override
+    public int operLessThan(String color, int cotton_part) {
+        return sockRepository.operLessThan(color, cotton_part);
+    }
+
+    @Override
+    public int operEqual(String color, int cotton_part) {
+        return sockRepository.operEqual(color, cotton_part);
+    }
+
+    @Override
+    public List<Sock> testAll() {
+        return sockRepository.findAll();
+    }
+}
