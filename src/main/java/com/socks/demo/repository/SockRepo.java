9b4baa5a6397d71c0cff0158445cd7d74c94f840
ivanborodkin1997 <boroda0360@mail.ru>
2021-10-28 11:52:52 +0300

    Update project. Fixed bugs

--- src/main/java/com/socks/demo/repository/SockRepo.java
+++ src/main/java/com/socks/demo/repository/SockRepo.java
@@ -18,13 +18,13 @@ public interface SockRepo extends JpaRepository<Sock, Long> {
     @Query("update Sock s set s.quantity = s.quantity -?2 where s.color = ?1 and s.cottonPart =?3 ")
     void outcomeSocks(String color, Integer quantity, Integer cottonPart);
 
-    @Query("SELECT SUM(s.quantity) from Sock s where s.color =?1 and s.cottonPart > ?2")
+    @Query("select sum(s.quantity) from Sock s where s.color =?1 and s.cottonPart > ?2")
     Integer calcOperationMore(String color, Integer cottonPart);
 
-    @Query("SELECT SUM(s.quantity) from Sock s where s.color =?1 and s.cottonPart < ?2")
+    @Query("select sum(s.quantity) from Sock s where s.color =?1 and s.cottonPart < ?2")
     Integer calcOperationLess(String color, Integer cottonPart);
 
-    @Query("SELECT SUM(s.quantity) from Sock s where s.color =?1 and s.cottonPart = ?2")
+    @Query("select sum(s.quantity) from Sock s where s.color =?1 and s.cottonPart = ?2")
     Integer calcOperationEquals(String color, Integer cottonPart);
 
     Sock findSockByColorAndCottonPart(String color, Integer cottonPart);
