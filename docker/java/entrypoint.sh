54aa97c9c41e0a6328bdf2b0826d42ad8b849605
Roman Nikitin <n75rom@gmail.com>
2021-10-29 14:41:53 +0900

    Edit files of servera
    Fixed a issue with launching jar.

--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-echo -e "\e[1;95mlaunch project building"
-cd /init-serv/api-test-task
-echo -e "\e[1;95mmvn package"
+#
+cd /init-serv/cib-interns-test-task-v.1.0.0-rc0/
 mvn package
-echo -e "\e[1;95java -jar .jar"
-java -jar /init-serv/api-test-task/target/api-test-task-0.0.1-SNAPSHOT.jar
+java -jar /init-serv/cib-interns-test-task-v.1.0.0-rc0/target/api-test-task-0.0.1-SNAPSHOT.jar
