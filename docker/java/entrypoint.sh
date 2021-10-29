aa8c28bea461f401e52bae1f9a5fdc924b4e8694
Roman Nikitin <n75rom@gmail.com>
2021-10-29 14:41:53 +0900

    Edit files of server
    Fixed a issue with launching jar.

--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
 cd /init-serv/cib-interns-test-task-v.1.0.0-rc0/
+tree
 mvn package
-java -jar /init-serv/cib-interns-test-task-v.1.0.0-rc0/target/api-test-task-0.0.1-SNAPSHOT.jar
+tree
+echo "pwd: $(pwd)"
+java -jar '/init-serv/cib-interns-test-task-v.1.0.0-rc0/target/api-test-task-0.0.1-SNAPSHOT.jar'
