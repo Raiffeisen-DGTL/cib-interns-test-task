8febb04624956f7dc8ed561d6c4b32fcdc20b72e
Roman Nikitin <n75rom@gmail.com>
2021-10-30 17:54:07 +0900

    Edit files for deploying

--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #
+echo -e "\e[1;92mStart enttypoint.sh"
+tree /init-serv && tree /root/.serv
+cd /init-serv
+wget https://github.com/niki75jr/cib-interns-test-task/archive/refs/tags/v.2.0.0-rc0.tar.gz -O - | tar -xz
 cd /init-serv/cib-interns-test-task-v.2.0.0-rc0/
-tree
-mvn package
-tree
+echo -e "\e[1;91m $(tree)"
 echo "pwd: $(pwd)"
 echo -e "\e[1;95mBuildVersion: \e[1;92m""1.0.0-dev0"
-java -jar '/init-serv/cib-interns-test-task-v.1.0.0-dev0/target/api-test-task-2.0.0-rc0.jar'
+mvn package
+tree /init-serv
+java -jar '/init-serv/cib-interns-test-task-v.2.0.0-rc0/target/api-test-task-2.0.0-rc0.jar'
