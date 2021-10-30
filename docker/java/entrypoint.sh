3afb32f15aa1e6daa5ae95dab50b7ab48d88784d
Roman Nikitin <n75rom@gmail.com>
2021-10-30 12:36:15 +0900

    Edit docker`s files

--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-cd /init-serv/cib-interns-test-task-v.1.0.0-rc2/
-echo -e "\e[1;95mBuildVersion: \e[1;92m""1.0.0-rc2"
+cd /init-serv/cib-interns-test-task-v.1.0.0-dev0/
+echo -e "\e[1;95mBuildVersion: \e[1;92m""1.0.0-dev0"
 tree
 mvn package
 tree
 echo "pwd: $(pwd)"
-echo -e "\e[1;95mBuildVersion: \e[1;92m""1.0.0-rc2"
-java -jar '/init-serv/cib-interns-test-task-v.1.0.0-rc2/target/api-test-task-0.0.1-SNAPSHOT.jar'
+echo -e "\e[1;95mBuildVersion: \e[1;92m""1.0.0-dev0"
+java -jar '/init-serv/cib-interns-test-task-v.1.0.0-dev0/target/api-test-task-0.0.1-SNAPSHOT.jar'
