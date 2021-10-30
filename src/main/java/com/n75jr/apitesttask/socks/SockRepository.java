ac3f62b3e48ccba372c35b91d90740a366d99b0a
Roman Nikitin <n75rom@gmail.com>
2021-10-30 13:29:37 +0900

    Edit GET /api/socks

--- src/main/java/com/n75jr/apitesttask/socks/SockRepository.java
+++ src/main/java/com/n75jr/apitesttask/socks/SockRepository.java
@@ -19,12 +19,12 @@ public interface SockRepository extends JpaRepository<Sock, Long> {
     @Query(value = "DELETE FROM socks WHERE color = :color AND cotton_part = :cotton_part AND quantity = :quantity", nativeQuery = true)
     int outcome(@Param("color") String col, @Param("cotton_part") int cottonPart, @Param("quantity") int quantity);
 
-    @Query(value = "SELECT count(*) FROM socks WHERE color = :color AND cotton_part > :cotton_part", nativeQuery = true)
+    @Query(value = "SELECT quantity FROM socks WHERE color = :color AND cotton_part > :cotton_part", nativeQuery = true)
     int operMoreThan(@Param("color") String color, @Param("cotton_part") int cotton_part);
 
-    @Query(value = "SELECT count(*) FROM socks WHERE color = :color AND cotton_part < :cotton_part", nativeQuery = true)
+    @Query(value = "SELECT quantity FROM socks WHERE color = :color AND cotton_part < :cotton_part", nativeQuery = true)
     int operLessThan(@Param("color") String color, @Param("cotton_part") int cotton_part);
 
-    @Query(value = "SELECT count(*) FROM socks WHERE color = :color AND cotton_part = :cotton_part", nativeQuery = true)
+    @Query(value = "SELECT quantity FROM socks WHERE color = :color AND cotton_part = :cotton_part", nativeQuery = true)
     int operEqual(@Param("color") String color, @Param("cotton_part") int cotton_part);
 }
