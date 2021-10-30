fac95a6bb2d978990799d1070ef6e331a0ac69e1
Roman Nikitin <n75rom@gmail.com>
2021-10-30 17:43:30 +0900

    Change version label

--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
-cd /init-serv/cib-interns-test-task-v.1.0.0-dev0/
-echo -e "\e[1;95mBuildVersion: \e[1;92m""1.0.0-dev0"
+cd /init-serv/cib-interns-test-task-v.2.0.0-rc0/
 tree
 mvn package
 tree
 echo "pwd: $(pwd)"
 echo -e "\e[1;95mBuildVersion: \e[1;92m""1.0.0-dev0"
-java -jar '/init-serv/cib-interns-test-task-v.1.0.0-dev0/target/api-test-task-0.0.1-SNAPSHOT.jar'
+java -jar '/init-serv/cib-interns-test-task-v.1.0.0-dev0/target/api-test-task-2.0.0-rc0.jar'
