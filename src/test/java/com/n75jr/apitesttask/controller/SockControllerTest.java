105b6dc9cacb1b94cf29a23fb81022d1c28b9531
Roman Nikitin <n75rom@gmail.com>
2021-11-03 00:11:42 +0900

    Add postIncomeWhenSockExists to SockControllerTest

--- src/test/java/com/n75jr/apitesttask/controller/SockControllerTest.java
+++ src/test/java/com/n75jr/apitesttask/controller/SockControllerTest.java
@@ -3,7 +3,9 @@ package com.n75jr.apitesttask.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.n75jr.apitesttask.dao.SockRepository;
 import com.n75jr.apitesttask.model.Sock;
+import com.n75jr.apitesttask.model.SockID;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -71,5 +73,26 @@ public class SockControllerTest {
         )
                 .andExpect(status().isBadRequest())
                 .andExpect(content().json(objectMapper.writeValueAsString(sock)));
+    }
+
+    @Test
+    public void postIncomeWhenSockExists() throws Exception {
+        int addQuantity = 100;
+        Sock sock = new Sock("green", 75, 10);
+        Sock addedSock = new Sock(sock.getColor(), sock.getCottonPart(), addQuantity);
+        Sock expectedSock = new Sock(sock.getColor(), sock.getCottonPart(), sock.getQuantity() + addQuantity);
+        SockID id = new SockID(sock.getColor(), sock.getCottonPart());
+
+        Mockito.when(repository.existsById(id)).thenReturn(true);
+        Mockito.when(repository.getById(id)).thenReturn(sock);
+        Mockito.when(repository.save(sock)).thenReturn(expectedSock);
+
+        mvc.perform(MockMvcRequestBuilders.
+                post("/api/socks/income")
+                .content(objectMapper.writeValueAsString(addedSock))
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(expectedSock)));
     }
 }
