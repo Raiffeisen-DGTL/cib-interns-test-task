b088f23c53341299e4751c13b121d0c729c5dfb2
Roman Nikitin <n75rom@gmail.com>
2021-11-02 23:47:27 +0900

    Add helper method isValidSock to SockController

--- src/main/java/com/n75jr/apitesttask/socks/SockController.java
+++ src/main/java/com/n75jr/apitesttask/socks/SockController.java
@@ -1,5 +1,6 @@
 package com.n75jr.apitesttask.socks;
 
+import com.n75jr.apitesttask.model.Sock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +11,16 @@ public class SockController {
     @Autowired
     private SockRepository sockRepository;
 
+    // helper methods:
+    private boolean isValidSock(Sock sock) {
+        boolean result = true;
+        if (sock.getColor() == null || sock.getCottonPart() == 0 || sock.getQuantity() == 0) {
+            return false;
+        }
+        if (sock.getCottonPart() <= 0 || sock.getCottonPart() > 100 || sock.getQuantity() <= 0) {
+            return false;
+        }
+        return result;
+    }
+    //
 }
