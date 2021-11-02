e837e6ee026f6f97b126a432891037779d3e8b1c
Roman Nikitin <n75rom@gmail.com>
2021-11-03 02:28:46 +0900

    Update .sh files for deploying

--- docker/db/src/entrypoint.sh
+++ docker/db/src/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #default entrypoint
 /bin/bash /usr/local/bin/docker-entrypoint.sh postgres
+# for healthcheck
 while [ $(pg_isready -U postgres -q; echo $?) -ne 0 ]; do
 	#statements
 	sleep 1
-	echo -e '\e[1;33min While'
+	echo -e '\e[1;33awaiting launch'
 done
 # after while
 createdb -U postgres socks && psql -d socks -f ./src/init.sql &> /dev/null
-echo -e "\e[1;95mBuildVersion: \e[1;92m""2.0.0-rc0"
+echo -e "\e[1;95mBuildVersion: \e[1;92m""3.0.0-rc0"
--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-echo -e "\e[1;92mStart enttypoint.sh"
+echo -e "\e[1;92mStart entrypoint.sh"
 tree /init-serv && tree /root/.serv
 cd /init-serv
-wget https://github.com/niki75jr/cib-interns-test-task/archive/refs/tags/v.2.0.0-rc0.tar.gz -O - | tar -xz
-cd /init-serv/cib-interns-test-task-v.2.0.0-rc0/
+wget https://github.com/niki75jr/cib-interns-test-task/archive/refs/tags/v.3.0.0-rc0.tar.gz -O - | tar -xz
+cd /init-serv/cib-interns-test-task-v.3.0.0-rc0/
 echo -e "\e[1;91m $(tree)"
 echo "pwd: $(pwd)"
-echo -e "\e[1;95mBuildVersion: \e[1;92m""1.0.0-dev0"
+echo -e "\e[1;95mBuildVersion: \e[1;92m""3.0.0-rc0"
 mvn package
 tree /init-serv
-java -jar '/init-serv/cib-interns-test-task-v.2.0.0-rc0/target/api-test-task-2.0.0-rc0.jar'
+java -jar '/init-serv/cib-interns-test-task-v.3.0.0-rc0/target/api-test-task-3.0.0-rc0.jar'
