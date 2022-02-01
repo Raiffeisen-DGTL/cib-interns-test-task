688d85e6aa059278cdf86ec0c80205bf19d4b94e
tmx69 <karpovartyom69@gmail.com>
2022-02-01 19:06:32 +0300

    Changed CustomExceptionAdvice

--- src/main/java/com/example/socks/Util/Response.java
+++ src/main/java/com/example/socks/Util/Response.java
@@ -1,7 +1,6 @@
 package com.example.socks.Util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 public class Response {
     private String message;
 
@@ -11,5 +10,29 @@ public class Response {
     public Response(String message, String debugMessage) {
         this.message = message;
         this.debugMessage = debugMessage;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDebugMessage() {
+        return debugMessage;
+    }
+
+    public void setDebugMessage(String debugMessage) {
+        this.debugMessage = debugMessage;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "message='" + message + '\'' +
+                ", debugMessage='" + debugMessage + '\'' +
+                '}';
     }
 }
