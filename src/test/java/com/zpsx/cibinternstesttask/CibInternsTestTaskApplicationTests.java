417a718ef5d1fa69af35ab1b9fc43affb2d282b0
zeropointsix <logvinov.k.a.98@gmail.com>
2022-03-23 13:10:56 +0300

    Init empty project

---
+++ src/test/java/com/zpsx/cibinternstesttask/CibInternsTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpsx.cibinternstesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibInternsTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
